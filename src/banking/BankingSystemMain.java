f951beb53ab9a917f7eca088da342e1fb4129a27
creative00 <fromdawnynight@gmail.com>
2022-11-08 17:48:09 +0900

    룰라랄라

--- src/banking/BankingSystemMain.java
+++ src/banking/BankingSystemMain.java
@@ -20,6 +20,7 @@ public class BankingSystemMain implements ICustomDefine {
 	public static void main(String[] args) {
 		
 		System.out.println("1차프로젝트(학원)");
+		System.out.println("pull없이 작업진행함");
 		
 		AccountManager manager = new AccountManager();
 		Scanner scan = new Scanner(System.in);
